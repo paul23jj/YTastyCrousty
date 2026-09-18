8e8adfe90216703ecd519349b3bc52382ded5046
Paul Gaulmin <paul.gaulmin@ynov.com>
2026-09-18 11:05:58 +0200

    chore: database.py

--- src/ytastycrousty/db/database.py
+++ src/ytastycrousty/db/database.py
@@ -1,5 +1,30 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from .config import settings
 
+engine = create_async_engine(settings.db_url, echo=True)
+
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
